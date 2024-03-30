5e13751313ecf275c34e13b8e7906ef996705dd4
Tyler <halltj.260@gmail.com>
2024-03-30 14:49:46 -0400

    march 30 main change

--- source/main.c
+++ source/main.c
@@ -16,41 +16,151 @@
 /* NOTE: int return values can be used to indicate errors (typically non-zero)
    or success (typically zero return value) */
 
-/* TODO: change this return type */
-void indexPage(const char* url);
+char* indexPage(const char* url);
 
-int addWordOccurrence(const char* word, const int wordLength
-		       /* TODO: other parameters you need */);
+int addWordOccurrence(const char* word, const int wordLength);
 
-void printTrieContents(/* TODO: any parameters you need */);
+void printTrieContents(trieNode *root);
 
-int freeTrieMemory(/* TODO: any parameters you need */);
+int freeTrieMemory(trieNode* root);
 
 int getText(const char* srcAddr, char* buffer, const int bufSize);
 
-int main(int argc, char** argv){
-  /* TODO: write the (simple) main function
+int main(int argc, char** argv) {
+    // Check if the URL argument is provided
+    if (argc < 2) {
+        fprintf(stderr, "Use: %s <URL>\n", argv[0]);
+        return -1; // Return non-zero to indicate failure
+    }
 
-  /* argv[1] will be the URL to index, if argc > 1 */
+    // Call a function that indexes the web page and returns a pointer to the root of the trie
+    trieNode* root = indexPage(argv[1]);
+    if (root == NULL) {
+        fprintf(stderr, "Failed to index the web page\n");
+        return -1; // Return non-zero to indicate failure
+    }
 
-  return 0;
+    // Call a function that prints out the counts of all the words in the trie
+    printTrieContents(root);
+
+    // Call a function to destroy the trie (i.e., free any allocated memory) when it is no longer needed
+    freeTrieMemory(root);
+
+    return 0; // Return zero to indicate success
 }
+
 
 /* TODO: define the functions corresponding to the above prototypes */
 
-/* TODO: change this return type */
-void indexPage(const char* url)
-{}
 
-int addWordOccurrence(const char* word, const int wordLength
-		       /* TODO: other parameters you need */)
-{}
+/*
+indexPage function:
+-struct node *indexPage(const char *url);
+-get the content of a page(getText());	//if zero there is no content
+-create a root node
+-analyze the content(loop)
+	.find a word		//calculate_sum.c
+	.addWordOccurrence(...); //recursive 
+-return to root
+*/
 
-void printTrieContents(/* TODO: any parameters you need */)
-{}
+trieNode* indexPage(const char* url) {
 
-int freeTrieMemory(/* TODO: any parameters you need */)
-{}
+
+    // Store only first 300000 char 
+    char buffer[300001]; // +1 for null terminator
+
+    // check for text
+    int bytesRead = getText(url, buffer, sizeof(buffer));
+    if (bytesRead <= 0) {
+        fprintf(stderr, "Failed to retrieve text from URL: %s\n", url);
+        return NULL; // Return NULL to indicate failure
+    }
+
+    // create root node
+    trieNode* root = malloc(sizeof(trieNode));
+    if (root == NULL) {
+        fprintf(stderr, "Memory allocation failed for trie root\n");
+        return NULL; // Return NULL to indicate failure
+    }
+    root->parent = NULL;
+    root->visits = 0;
+    for (int i = 0; i < children_count; i++) {
+        root->children[i] = NULL;
+    }
+
+    // Analyze the content and add words to the trie
+    char* word = strtok(buffer, " \t\n\r.,;:!?'\"()[]{}0123456789");
+    while (word != NULL) {
+        // Clean the word and convert it to lowercase
+        char* cleanWord = malloc(strlen(word) + 1);
+        if (cleanWord == NULL) {
+            fprintf(stderr, "Memory allocation failed for cleanWord\n");
+            freeTrieMemory(root); // Free allocated memory before returning
+            return NULL;
+        }
+        int j = 0;
+        for (int i = 0; word[i] != '\0'; i++) {
+            if (isalpha(word[i])) {
+                cleanWord[j++] = tolower(word[i]);
+            }
+        }
+        cleanWord[j] = '\0';
+
+        // add the clean word to the trie
+        addWordOccurrence(cleanWord, strlen(cleanWord), root);
+
+        // free memory allocated for cleanWord
+        free(cleanWord);
+
+        // next word
+        word = strtok(NULL, " \t\n\r.,;:!?'\"()[]{}0123456789");
+    }
+
+    return root; // Return the root node of the trie
+}
+
+
+int addWordOccurrence(const char* word, const int wordLength, trieNode* root){
+  
+  // is word empty
+    if (word == NULL || wordLength == 0) {
+        return 0; // fail
+    }
+
+    // Add the word to the trie
+    trieNode* node = visitNode(word, root);
+    if (node != NULL) {
+        node->visits++;
+        return 1; // pass
+    } else {
+        return 0; // fail
+    }
+}
+
+
+
+void printTrieContents(trieNode* root){
+  
+  if(root != NULL){
+    printTrie(stdout, root);
+  }
+  
+  else
+  return 0;
+  
+}
+
+int freeTrieMemory(trieNode* root ){
+  
+  if(root != NULL){
+  freeNode(root);
+  }
+
+  else
+  return 0;
+
+}
 
 /* You should not need to modify this function */
 int getText(const char* srcAddr, char* buffer, const int bufSize){
